7f0244c77890bcb0ae682cf2a7369c478da1981c
Хайрбек Мамбетов <mnocard@gmail.com>
2021-06-16 12:03:16 +0500

    Запрос информации по аккаунту

--- DevinoTest/Dto/AccountDto.cs
+++ DevinoTest/Dto/AccountDto.cs
@@ -1,28 +1,24 @@
-﻿
-namespace DevinoTest.Dto
+﻿public class AccountRootobject
 {
-    public class AccountDto
-    {
-        public int CompanyId { get; set; }
+    public AccountResult result { get; set; }
+}
 
-        public string AccountType { get; set; }
+public class AccountResult
+{
+    public AccountDto account { get; set; }
+    public object[] validPacketChannels { get; set; }
+}
 
-        public decimal Balance { get; set; }
-
-        public decimal Credit { get; set; }
-
-        public decimal ReserveSms { get; set; }
-
-        public decimal ReserveViber { get; set; }
-
-        public decimal Reserve { get; set; }
-
-        public int CurrencyId { get; set; }
-
-        public bool IsBlocked { get; set; }
-
-        public decimal DisableThreshold { get; set; }
-
-        public decimal NotifyThreshold { get; set; }
-    }
+public class AccountDto
+{
+    public int companyId { get; set; }
+    public string accountType { get; set; }
+    public decimal balance { get; set; }
+    public decimal credit { get; set; }
+    public decimal reserveSms { get; set; }
+    public decimal reserveViber { get; set; }
+    public decimal reserve { get; set; }
+    public int currencyId { get; set; }
+    public bool isBlocked { get; set; }
+    public decimal notifyThreshold { get; set; }
 }
--- DevinoTest/Program.cs
+++ DevinoTest/Program.cs
@@ -13,52 +13,43 @@ namespace DevinoTest
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private const string _AccountApi = "/billing-api/companies/current/account";
+        private const string _SendSmsApi = "/sms/messages";
+        private const string _SmsStatusApi = "/sms-stat/statuses/get-by-ids";
 
         static async Task Main(string[] args)
         {
-
-
-            await ProcessRepositories();
-            Console.WriteLine("Devino test");
-            Console.ReadKey();
-        }
-
-        private static async Task ProcessRepositories()
-        {
-            //string key = 
-            //var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            //return Convert.ToBase64String(plainTextBytes);
-
-
             client.BaseAddress = new Uri("https://api.devino.online");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Authorization", "Key 04ee2b47-3a97-4134-98d7-dd21a5223ea6");
 
+            var account = await GetAccountInfoAsync();
+            Console.WriteLine($"Ваш баланс составляет: {account.balance} руб.");
 
 
+            Console.WriteLine("Devino test");
+            Console.ReadKey();
+        }
+
+        private static async Task<AccountDto> GetAccountInfoAsync()
+        {
+            AccountRootobject account = null;
             try
             {
-                var account = await GetBalanceAsync("/billing-api/companies/current/account");
-                Console.WriteLine(account.Balance);
+                HttpResponseMessage response = await client.GetAsync(_AccountApi);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    account = JsonConvert.DeserializeObject<AccountRootobject>(jsonString);
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        private static async Task<AccountDto> GetBalanceAsync(string path)
-        {
-            AccountDto account = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonString = await response.Content.ReadAsStringAsync();
-                account = JsonConvert.DeserializeObject<AccountDto>(jsonString);
-            }
-            return account;
+            return account.result.account;
         }
     }
 }
